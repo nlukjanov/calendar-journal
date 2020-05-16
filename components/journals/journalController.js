fafd5c83c41a5453d75738d62a3bd84a8e33fce0
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-16 19:14:23 +0100

    refactored controllers to es6

--- components/journals/journalController.js
+++ components/journals/journalController.js
@@ -1,63 +1,59 @@
 const Journal = require('./journalModel');
 
-function index(req, res) {
-  Journal.find()
-    .then((entries) => {
-      return res.status(200).json(entries);
-    })
-    .catch((err) => {
-      return res
-        .status(500)
-        .json({ message: 'somethings is wrong', error: err.message });
-    });
+async function index(req, res) {
+  try {
+    const entries = await Journal.find();
+    return res.status(200).json(entries);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: 'somethings is wrong', error: err.message });
+  }
 }
 
-function create(req, res) {
-  Journal.create(req.body)
-    .then((journalEntry) => {
-      return res
-        .status(201)
-        .json({ message: `Journal entry ${journalEntry.title} created` });
-    })
-    .catch((err) => {
-      return res
-        .status(422)
-        .json({ message: 'somethings is wrong', error: err.message });
-    });
+async function create(req, res) {
+  try {
+    const journalEntry = await Journal.create(req.body);
+    return res
+      .status(201)
+      .json({ message: `Journal entry ${journalEntry.title} created` });
+  } catch (err) {
+    return res
+      .status(422)
+      .json({ message: 'somethings is wrong', error: err.message });
+  }
 }
 
-function edit(req, res) {
-  Journal.findById(req.params.id)
-    .then((foundEntry) => {
-      if (!foundEntry)
-        return res.status(404).json({ message: 'entry not found' });
-      // here check if user is logged in
-      // check if user is owner
-      foundEntry.set(req.body);
-      foundEntry.save();
-      return res.status(202).json(foundEntry);
-    })
-    .catch((err) => {
-      console.log(err);
-      res
-        .status(422)
-        .json({ message: 'somethings is wrong', error: err.message });
-    });
+async function edit(req, res) {
+  try {
+    const foundEntry = await Journal.findById(req.params.id);
+    if (!foundEntry)
+      return res.status(404).json({ message: 'entry not found' });
+    // here check if user is logged in
+    // check if user is owner
+    foundEntry.set(req.body);
+    foundEntry.save();
+    return res.status(202).json(foundEntry);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(422)
+      .json({ message: 'somethings is wrong', error: err.message });
+  }
 }
 
-function destroy(req, res) {
-  Journal.findById(req.params.id)
-    .then((foundEntry) => {
-      if (!foundEntry)
-        return res.status(404).json({ message: 'entry not found' });
-      foundEntry.deleteOne();
-      return res.status(204).json({ message: 'delete' });
-    })
-    .catch((err) => {
-      return res
-        .status(500)
-        .json({ message: 'somethings is wrong', error: err.message });
-    });
+async function destroy(req, res) {
+  try {
+    const foundEntry = await Journal.findById(req.params.id);
+    if (!foundEntry)
+      return res.status(404).json({ message: 'entry not found' });
+    foundEntry.deleteOne();
+    return res.status(204).json({ message: 'delete' });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: 'somethings is wrong', error: err.message });
+  }
 }
 
 module.exports = { index, create, edit, destroy };
--- components/users/userController.js
+++ components/users/userController.js
@@ -2,50 +2,48 @@ const User = require('./userModel');
 const jwt = require('jsonwebtoken');
 const { secret } = require('../../config/environment');
 
-function register(req, res) {
-  User.create(req.body)
-    .then((user) =>
-      res
-        .status(201)
-        .json({ message: `Thank you for registering ${user.username}` })
-    )
-    .catch((err) => {
-      return res
-        .status(422)
-        .json({ message: 'somethings is wrong', error: err.message });
-    });
+async function register(req, res) {
+  try {
+    const user = await User.create(req.body);
+    res
+      .status(201)
+      .json({ message: `Thank you for registering ${user.username}` });
+  } catch (err) {
+    return res
+      .status(422)
+      .json({ message: 'somethings is wrong', error: err.message });
+  }
 }
 
-function login(req, res) {
-  User.findOne({ username: req.body.username })
-    .then((user) => {
-      if (!user || !user.validatePassword(req.body.password)) {
-        return res.status(401).json({ message: 'Unauthorized' });
-      }
-      const token = jwt.sign({ sub: user._id }, secret, {
-        expiresIn: '24h'
-      });
-      return res
-        .status(202)
-        .json({ message: `Welcome back ${user.username}`, token });
-    })
-    .catch((err) => {
-      console.log(err);
+async function login(req, res) {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user || !user.validatePassword(req.body.password)) {
       return res.status(401).json({ message: 'Unauthorized' });
+    }
+    const token = jwt.sign({ sub: user._id }, secret, {
+      expiresIn: '24h'
     });
+    return res
+      .status(202)
+      .json({ message: `Welcome back ${user.username}`, token });
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
 }
 
-function myjournal(req, res) {
-  User.findById(req.currentUser._id)
-    .populate('journalEntries')
-    .then((user) => {
-      if (!user) throw new Error('Unauthorized');
-      return res.status(200).json(user);
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(401).json({ message: 'Unauthorized' });
-    });
+async function myjournal(req, res) {
+  try {
+    const user = await User.findById(req.currentUser._id).populate(
+      'journalEntries'
+    );
+    if (!user) throw new Error('Unauthorized');
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
 }
 
 module.exports = { register, login, myjournal };
