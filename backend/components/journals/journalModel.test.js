7b4dc89eb2c2c411919486bea21786d78f6ea43a
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-21 19:36:49 +0100

    fix journal model test

--- backend/components/journals/journalModel.test.js
+++ backend/components/journals/journalModel.test.js
@@ -6,29 +6,26 @@ const Journal = require('./journalModel');
 
 describe('Journal model', () => {
   it('should throw error if author is not present', async () => {
-    expect.assertions(1);
     const journalEntryNoAuthor = new Journal({
       title: 'Hello'
     });
 
-    await expect(() => journalEntryNoAuthor.validate()).rejects.toThrow(
+    await expect(journalEntryNoAuthor.validate()).rejects.toThrow(
       'Journal validation failed: author: Path `author` is required.'
     );
   });
 
   it('should throw error if wrong data type is submitted', async () => {
-    expect.assertions(1);
     const journalEntryNoAuthor = new Journal({
       title: { a: 'b', c: 'd' }
     });
 
-    await expect(() => journalEntryNoAuthor.validate()).rejects.toThrow(
+    await expect(journalEntryNoAuthor.validate()).rejects.toThrow(
       'Journal validation failed: title: Cast to string failed for value "{ a: \'b\', c: \'d\' }" at path "title", author: Path `author` is required.'
     );
   });
 
   it('should throw error if title is not present', async () => {
-    expect.assertions(1);
     const user = new User({
       username: 'userNoPass',
       email: 'userNoPass',
@@ -40,7 +37,7 @@ describe('Journal model', () => {
       author: user
     });
 
-    await expect(() => journalEntryNoTitle.validate()).rejects.toThrow(
+    await expect(journalEntryNoTitle.validate()).rejects.toThrow(
       'Journal validation failed: title: Path `title` is required.'
     );
   });
