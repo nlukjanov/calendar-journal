34cc612a1a0b5803c36f9d7c06c51b79a68d0037
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-22 15:09:25 +0100

    setup testing library for cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+// eslint-disable-next-line node/no-unpublished-import
+import '@testing-library/cypress/add-commands';
