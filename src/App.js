071c56f0e71bea2dad4b478dfdc55239f75ecf1f
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-22 15:09:46 +0100

    add landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+const Landing = lazy(() => import('./components/Landing/Landing'));
 
 function App() {
   return (
-    <div data-testid='landing'>
-      Hello
-    </div>
+    <Suspense fallback={<div>Loading...</div> }>
+      <BrowserRouter>
+        <main data-testid='main'>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Landing = () => {
+  return <div data-testid='landing'>Hello</div>;
+};
+
+export default Landing;
