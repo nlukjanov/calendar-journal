c4ded2dac106f56e31eaa77638bae79eea3c21d8
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-22 15:09:39 +0100

    add landing and nav tests

--- cypress/e2e/calendar-journal.js
+++ cypress/e2e/calendar-journal.js
@@ -1,6 +1,14 @@
 /* eslint-disable jest/expect-expect */
 describe('calendar journal app', () => {
   it('should start at the landing page', () => {
-    cy.visit('/').get('[data-testid=landing]').should('have.text', 'Hello');
+    cy.visit('/');
+    cy
+      .findByTestId('landing')
+      .should('have.text', 'Hello')
+      .findByTestId('nav')
+      .findByTestId('signin')
+      .should('have.text', 'Sign in')
+      .findByTestId('signup')
+      .should('have.text', 'Sign up');
   });
 });
