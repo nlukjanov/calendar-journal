35f47ec432c4677b3ba48346285ef0c6f84f2161
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-24 11:49:01 +0100

    add cors to express app

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const logger = require('./lib/logger');
 const router = require('./config/router');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(logger);
 
-app.use(router);
+app.use('/api',router);
 
 app.get('/', (req, res) => res.send('Hello World'));
 
