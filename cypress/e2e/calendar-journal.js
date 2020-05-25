a359007435ace6c252bdb1ae08b99b43f313bb04
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-05-25 10:51:12 +0100

    add myJournal

--- cypress/e2e/calendar-journal.js
+++ cypress/e2e/calendar-journal.js
@@ -41,4 +41,16 @@ describe('calendar journal app', () => {
     cy.findByText('Sign In').click();
     cy.location('pathname').should('eq', '/myjournal');
   });
+
+  it('should display User\'s journal entries', () => {
+    cy.visit('/signin');
+    cy.findByLabelText('Email');
+    cy.findByLabelText('Password');
+    cy.findByPlaceholderText('Email').type('nik@mail.com');
+    cy.findByPlaceholderText('Password').type('pass');
+    cy.findByText('Sign In').click();
+    cy.wait(500);
+    cy.location('pathname').should('eq', '/myjournal');
+    cy.findByTestId('title1').should('have.text', 'Title1');
+  });
 });
--- src/components/MyJournal/MyJournal.js
+++ src/components/MyJournal/MyJournal.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { getToken } from '../../lib/authHelper';
 
 const MyJournal = () => {
+  const [journalEntries, setJournalEntries] = useState();
+
+  useEffect(() => {
+    getEntries();
+  }, []);
+
+  const getEntries = async () => {
+    const headers = { headers: { Authorization: `Bearer ${getToken()}` } };
+    console.log(headers);
+    try {
+      const res = await axios.get(
+        'http://localhost:4000/api/myjournal',
+        headers
+      );
+      console.log(res);
+      setJournalEntries(res.body);
+      // add notification with res
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => console.log(journalEntries), [journalEntries]);
   return <div>My Journal</div>;
 };
 
