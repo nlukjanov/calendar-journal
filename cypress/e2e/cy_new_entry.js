1ee762847867d0647ac1bb5889d847ab60bafdf8
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-06-10 20:21:17 +0100

    add new entry tests

--- cypress/e2e/cy_new_entry.js
+++ cypress/e2e/cy_new_entry.js
@@ -4,8 +4,10 @@ describe('calendar journal app', () => {
     cy.visit('/new-entry');
     cy.findByText('New Journal Entry');
     cy.findByTestId('entry-form');
-    cy.findByPlaceholderText('Title').type('My entry title');
-    cy.findByPlaceholderText('Entry text').type('My entry text');
+    cy.findByLabelText('Title');
+    cy.findByPlaceholderText(/entry title/i).type('My entry title');
+    cy.findByLabelText('Entry text');
+    cy.findByPlaceholderText(/Entry text/i).type('My entry text');
     cy.findByText('Create Entry').click();
     cy.location('pathname').should('eq', '/myjournal');
     cy.contains('My entry title');
