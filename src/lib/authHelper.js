8ce3f8dc6e0f79bacff351d6fdb2c3687088a210
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-06-10 14:36:47 +0100

    fix isAuthenticated helper

--- src/lib/authHelper.js
+++ src/lib/authHelper.js
@@ -19,11 +19,10 @@ export const getPayload = () => {
 };
 
 export const isAuthenticated = () => {
-  const token = getToken();
-  if (!token) return false;
-  const parts = token.split('.');
-  if (parts.length < 3) return false;
-  return JSON.parse(atob(parts[1]));
+  const payload = this.getPayload();
+  if (!payload) return false;
+  const now = Math.round(Date.now() / 1000);
+  return now < payload.exp;
 };
 
 export const isOwner = (id) => {
