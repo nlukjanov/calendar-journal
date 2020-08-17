78dbbfc44723df35341584897c792e5281aeae1d
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-17 11:15:51 +0100

    adjust cypress landing page test

--- cypress/e2e/cy_landing_page.js
+++ cypress/e2e/cy_landing_page.js
@@ -8,15 +8,4 @@ describe('Landing page', () => {
     cy.findByText('Sign in');
     cy.findByText('Sign up');
   });
-
-  it('should display User\'s journal entries', () => {
-    cy.visit('/signin');
-    cy.findByLabelText('Email');
-    cy.findByLabelText('Password');
-    cy.findByPlaceholderText('Email').type('nik@mail.com');
-    cy.findByPlaceholderText('Password').type('pass');
-    cy.findByText('Sign In').click();
-    cy.location('pathname').should('eq', '/myjournal');
-    cy.findByTestId('title1').should('have.text', 'Title1');
-  });
 });
