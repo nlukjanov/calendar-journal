6bfa9986f838e135cb9ed7ca0c64f030c522a7f5
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-17 11:27:13 +0100

    adjust cy my journal test

--- cypress/e2e/cy_my_journal.js
+++ cypress/e2e/cy_my_journal.js
@@ -1,14 +1,14 @@
 /* eslint-disable jest/expect-expect */
 describe('My Journal', () => {
-  it('should display User\'s journal entries', () => {
-    cy.visit('/signin');
-    cy.findByLabelText('Email');
-    cy.findByLabelText('Password');
-    cy.findByPlaceholderText('Email').type('nik@mail.com');
-    cy.findByPlaceholderText('Password').type('pass');
-    cy.findByText('Sign In').click();
-    cy.wait(500);
-    cy.location('pathname').should('eq', '/myjournal');
-    cy.findByTestId('title1').should('have.text', 'Title1');
+  it("should display User's journal entries", () => {
+    // eslint-disable-next-line jest/valid-expect-in-promise
+    cy.request('POST', 'http://localhost:4000/api/login', {
+      email: 'nik@email.com',
+      password: 'pass'
+    }).then((response) => {
+      window.localStorage.setItem('token', response.body.token);
+    });
+    cy.visit('/myjournal')
+    cy.findByText('My journal').should('have.text', 'My journal');
   });
 });
