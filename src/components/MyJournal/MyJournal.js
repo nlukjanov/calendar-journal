54d750a49b64775094105bf07c19b46ad0ab0182
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-09-28 19:42:56 +0100

    feat: add calendar to myjournal page

--- src/components/MyJournal/MyJournal.js
+++ src/components/MyJournal/MyJournal.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { getToken } from '../../lib/authHelper';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 const MyJournal = () => {
+  const headerToolbarOptions = {
+    start: 'title', // will normally be on the left. if RTL, will be on the right
+    center: '',
+    end: 'today prevYear prev,next nextYear' // will normally be on the right. if RTL, will be on the left
+  };
   const [journalEntries, setJournalEntries] = useState();
-
   useEffect(() => {
     getEntries();
   }, []);
@@ -35,6 +41,15 @@ const MyJournal = () => {
             );
           })}
       </div>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView='dayGridMonth'
+        headerToolbar={headerToolbarOptions}
+        events={[
+          { title: 'event 1', date: '2020-10-02' },
+          { title: 'event 2', date: '2020-10-02' }
+        ]}
+      />
     </>
   );
 };
