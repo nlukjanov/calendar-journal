0fd0655c0ed96fb48a194d828906d5b71229a110
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-01 20:55:41 +0100

    feat: add lint staged and husky pre-commit

--- backend/app.test.js
+++ backend/app.test.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const request = require('supertest');
 
 describe('index test', () => {
-  it('calling / should return Hello World ', async () => {
+  it('calling / should return Hello World', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual('Hello World');
