02cbc406f4c5eedd223aa192343749c1fcad0869
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-10 10:01:10 +0100

    feat: add date to journal model

--- backend/components/journals/journalModel.js
+++ backend/components/journals/journalModel.js
@@ -4,12 +4,13 @@ const journalSchema = new mongoose.Schema(
   {
     author: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
-    entryText: { type: String }
+    entryText: { type: String },
+    date: { type: Date },
   },
   {
-    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
   },
-  { strict: 'throw' }
+  { strict: 'throw' },
 );
 
 module.exports = mongoose.model('Journal', journalSchema);
