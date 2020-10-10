7517a4e31eb5bc4f8a8eecede772811a808d11d9
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-10 09:49:51 +0100

    feat: add journal model date failing test

--- backend/components/journals/journalModel.test.js
+++ backend/components/journals/journalModel.test.js
@@ -7,38 +7,76 @@ const Journal = require('./journalModel');
 describe('Journal model', () => {
   it('should throw error if author is not present', async () => {
     const journalEntryNoAuthor = new Journal({
-      title: 'Hello'
+      title: 'Hello',
     });
 
     await expect(journalEntryNoAuthor.validate()).rejects.toThrow(
-      'Journal validation failed: author: Path `author` is required.'
+      'Journal validation failed: author: Path `author` is required.',
     );
   });
 
   it('should throw error if wrong data type is submitted', async () => {
     const journalEntryNoAuthor = new Journal({
-      title: { a: 'b', c: 'd' }
+      title: { a: 'b', c: 'd' },
     });
 
     await expect(journalEntryNoAuthor.validate()).rejects.toThrow(
-      'Journal validation failed: title: Cast to string failed for value "{ a: \'b\', c: \'d\' }" at path "title", author: Path `author` is required.'
+      'Journal validation failed: title: Cast to string failed for value "{ a: \'b\', c: \'d\' }" at path "title", author: Path `author` is required.',
     );
   });
 
-  it('should throw error if title is not present', async () => {
-    const user = new User({
-      username: 'userNoPass',
-      email: 'userNoPass',
-      password: 'pass',
-      passwordConfirmation: 'pass'
+  let user;
+
+  describe('User actions with journal', () => {
+    beforeEach(async () => {
+      user = new User({
+        username: 'user',
+        email: 'user',
+        password: 'pass',
+        passwordConfirmation: 'pass',
+      });
+    });
+    it('should throw error if title is not present', async () => {
+      const journalEntryNoTitle = new Journal({
+        author: user,
+      });
+
+      await expect(journalEntryNoTitle.validate()).rejects.toThrow(
+        'Journal validation failed: title: Path `title` is required.',
+      );
     });
 
-    const journalEntryNoTitle = new Journal({
-      author: user
+    it('journal model should have entry text', () => {
+      const journalEntryWithText = new Journal({
+        author: user,
+        title: 'entry with title',
+        entryText: 'entry text',
+      });
+
+      expect(journalEntryWithText.entryText).toStrictEqual('entry text');
     });
 
-    await expect(journalEntryNoTitle.validate()).rejects.toThrow(
-      'Journal validation failed: title: Path `title` is required.'
-    );
+    it('journal model should have date field', () => {
+      const dateNow = new Date();
+      const journalEntryWithText = new Journal({
+        author: user,
+        title: 'entry with title',
+        entryText: 'entry text',
+        date: dateNow,
+      });
+
+      expect(journalEntryWithText.date).toStrictEqual(dateNow);
+    });
+
+    it('journal model should not accept fields different from set in the model', () => {
+      const journalEntryWithText = new Journal({
+        author: user,
+        title: 'entry with title',
+        entryText: 'entry text',
+        somethingElse: 'something else',
+      });
+
+      expect(journalEntryWithText.somethingElse).toStrictEqual(undefined);
+    });
   });
 });
