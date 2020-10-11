52aa535fd3d7e89fef721bd31e2705c6a4fb07eb
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-11 12:02:37 +0100

    feat: add conditional signin/signup and myjournal buttons

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
+import { isAuthenticated } from '../../lib/authHelper';
 
 const Landing = () => (
   <Jumbotron>
@@ -11,12 +12,21 @@ const Landing = () => (
       <h1>Welcome to Calendar Journal</h1>
       <p>Take your notes and keep your memories</p>
       <div className='d-flex justify-content-center'>
-        <Nav.Link as={Link} to='signin'>
-          <Button variant='primary'>Sign In</Button>
-        </Nav.Link>
-        <Nav.Link as={Link} to='/signup'>
-          <Button variant='primary'>Sign Up</Button>
-        </Nav.Link>
+        {!isAuthenticated() && (
+          <>
+            <Nav.Link as={Link} to='signin'>
+              <Button variant='primary'>Sign In</Button>
+            </Nav.Link>
+            <Nav.Link as={Link} to='/signup'>
+              <Button variant='primary'>Sign Up</Button>
+            </Nav.Link>
+          </>
+        )}
+        {isAuthenticated() && (
+          <Nav.Link as={Link} to='/myjournal'>
+            <Button variant='primary'>See my journal</Button>
+          </Nav.Link>
+        )}
       </div>
     </Container>
   </Jumbotron>
