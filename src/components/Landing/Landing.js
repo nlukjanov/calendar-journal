51b0b0c01d78aa8115a11e9b12c2b7a8be83aa27
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-11 11:20:19 +0100

    feat: refactor landing page to Jumbotron with buttons

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import { Link, withRouter } from 'react-router-dom';
 
-const Landing = () => {
-  return <div data-testid='landing'>Hello</div>;
-};
+const Landing = () => (
+  <Jumbotron>
+    <Container className='text-center'>
+      <h1>Welcome to Calendar Journal</h1>
+      <p>Take your notes and keep your memories</p>
+      <div className='d-flex justify-content-center'>
+        <Nav.Link as={Link} to='signin'>
+          <Button variant='primary'>Sign In</Button>
+        </Nav.Link>
+        <Nav.Link as={Link} to='/signup'>
+          <Button variant='primary'>Sign Up</Button>
+        </Nav.Link>
+      </div>
+    </Container>
+  </Jumbotron>
+);
 
 export default Landing;
