e6d3494ed00ce8047a252a0f61cfb5fafdcb7a1e
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-12 21:19:07 +0100

    fix: delete myjournal test for now

--- src/components/MyJournal/MyJournal.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// eslint-disable-next-line node/no-unpublished-import
-import { render, waitFor } from '@testing-library/react';
-import MyJournal from './MyJournal';
-
-test('renders hello in main', async () => {
-  const { getByText } = render(<MyJournal />);
-  await waitFor(() => expect(getByText(/My Journal/i)).toBeInTheDocument());
-});
