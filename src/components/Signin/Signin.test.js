a2a75a93d8600f9ec6989a09ad4e20b6f4ff1774
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-17 08:26:54 +0100

    fix: remove async from signin test

--- src/components/Signin/Signin.test.js
+++ src/components/Signin/Signin.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Signin from './Signin';
 
-test('renders new journal entry', async () => {
+test('renders new journal entry', () => {
   const { container } = render(<Signin />);
   expect(container).toMatchSnapshot();
 });
