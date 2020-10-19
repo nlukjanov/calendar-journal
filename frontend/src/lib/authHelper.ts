635f7244e7e35c8bac17bca50521885ab88587db
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-19 20:23:21 +0100

    fix: pass token as function parameter

--- frontend/src/lib/authHelper.ts
+++ frontend/src/lib/authHelper.ts
@@ -2,8 +2,8 @@ export const setToken = (token) => {
   localStorage.setItem('token', token);
 };
 
-export const getToken = () => {
-  return localStorage.getItem('token');
+export const getToken = (token) => {
+  return localStorage.getItem(token);
 };
 
 export const logout = () => {
